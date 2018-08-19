5927bf7494ada2a86a6b812d60b184e6f3c6e351
lixiang <lixiang01031995@163.com>
2018-08-19 15:31:45 +0800

    对test文件进行第一次更改

--- src/test.py
+++ src/test.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     b = np.array([[[2,2,2],[2,2,2],[2,2,2]],[[2,2,2],[2,2,2],[2,2,2]]])
     result = np.append(a, b,axis = 0)
     print result.shape
-    print "hello world"
+    print "the first modification"
